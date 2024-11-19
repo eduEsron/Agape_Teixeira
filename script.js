6e25f8f039135c9b9fea566670d9a3b5e97c442d
eduEsron <141090507+eduEsron@users.noreply.github.com>
2024-11-19 18:38:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
-let contato = document.getElementById("contatoLink")
-function getContato() {
-    if (window.innerWidth <= 600) {
-        contato.href = "https://www.instagram.com/menonita_agape/"
-    }
-}
-contato.addEventListener("click", getContato)
+let contato1 = document.getElementById("contato1")
+let contato2 = document.getElementById("contato2")
+let contato3 = document.getElementById("contato3")
+let contato4 = document.getElementById("contato4")
 window.addEventListener("load", function(){
     if (window.innerWidth <= 600) {
-    contato.textContent ="Instagram"
+    contato1.textContent = "Email"
+    contato2.textContent = "Grupo de Avisos"
+    contato3.textContent = "Instagram"
+    contato4.textContent = "Facebook"
     }
     else{
-        contato.textContent ="Contatos"
+    contato1.textContent = "Email: menonitaagapeteixeira@gmail.com"
+    contato2.textContent = "Grupo de Avisos: chat.whatsapp.com/GRbpBaswXWZ5Xe6CUC8X2C"
+    contato3.textContent = "Instagram: @menonita_agape"
+    contato4.textContent = "Facebook: Igreja Menonita Ágape - Teixeira Soares"
     }
 })
 window.addEventListener("resize", function(){
     if (window.innerWidth <= 600) {
     contato.textContent ="Instagram"
+    contato1.textContent = "Email"
+    contato2.textContent = "Grupo de Avisos"
+    contato3.textContent = "Instagram"
+    contato4.textContent = "Facebook"
     }
     else{
-        contato.textContent ="Contatos"
+        contato1.textContent = "Email: menonitaagapeteixeira@gmail.com"
+        contato2.textContent = "Grupo de Avisos: chat.whatsapp.com/GRbpBaswXWZ5Xe6CUC8X2C"
+        contato3.textContent = "Instagram: @menonita_agape"
+        contato4.textContent = "Facebook: Igreja Menonita Ágape - Teixeira Soares"
     }
 })
 
+//
